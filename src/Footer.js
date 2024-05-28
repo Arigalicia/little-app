3af2809b03b3dfdc755633c6740af72bc384a6a3
Arigalicia <128807022+Arigalicia@users.noreply.github.com>
2024-05-28 13:42:53 +0200

    Semantic Tags and Structure

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import longLogo from './images/LL-longLogo.PNG'
+
+function Footer() {
+    return(
+    <footer>
+<img src={longLogo} alt='long Logo'/>
+    </footer>
+    );
+    }
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from './images/Logo .svg'
+function Header(){
+return(
+<header>
+    <img src={Logo} alt='logo'></img>
+</header>
+);
+}
+
+export default Header;
