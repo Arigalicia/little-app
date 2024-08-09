7ecd152093328fbbd66224adadf5ec72112c6ded
Arigalicia <128807022+Arigalicia@users.noreply.github.com>
2024-08-09 14:46:00 +0200

    Last fixes

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,37 @@
 import { render, screen } from '@testing-library/react';
-import { initializeTimes, updateTimes } from './Main';
 
-describe('initializeTimes', () => {
-    it('should return the correct initial times', () => {
-        const initialTimes = initializeTimes();
-        expect(initialTimes).toEqual(expect.any(Array)); // Expecting an array
-        expect(initialTimes.length).toBeGreaterThan(0);  // Should have some times
-    });
+import BookingForm from './BookingForm';
+
+test('date input has required attribute', () => {
+    render(<BookingForm availableTimes={[]} submitForm={() => {}} />);
+    const dateInput = screen.getByLabelText(/Choose a Date/i);
+    expect(dateInput).toBeRequired();
+});
+
+test('time select has required attribute', () => {
+    render(<BookingForm availableTimes={['17:00']} submitForm={() => {}} />);
+    const timeSelect = screen.getByLabelText(/Choose a Time/i);
+    expect(timeSelect).toBeRequired();
+});
+
+test('guests input has required, min, and max attributes', () => {
+    render(<BookingForm availableTimes={[]} submitForm={() => {}} />);
+
+    const guestsInput = screen.getByLabelText(/number of guests/i);
+
+    expect(guestsInput).toBeRequired();
+    expect(guestsInput).toHaveAttribute('min', '1');
+    expect(guestsInput).toHaveAttribute('max', '10');
+});
+
+test('occasion select has required attribute and default option', () => {
+    render(<BookingForm availableTimes={[]} submitForm={() => {}} />);
+
+    const occasionSelect = screen.getByLabelText(/occasion/i);
+
+    expect(occasionSelect).toBeRequired();
+
+    const defaultOption = screen.getByText(/select an occasion/i);
+    expect(defaultOption).toBeInTheDocument();
+    expect(defaultOption).toHaveAttribute('value', '');
 });
